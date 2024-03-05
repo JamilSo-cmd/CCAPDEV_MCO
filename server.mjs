31bff4dfbb5089aa380e9cf7e25a0877b49ac2f5
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2024-03-05 16:36:25 +0800

    Moving of files and URL routing

--- server.mjs
+++ server.mjs
@@ -2,22 +2,77 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
-const http = require('http');
-const fs = require('fs');    
+import path from 'path';
+const __dirname = path.resolve();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-http.createServer(function (req,res){
+app.use(express.static('public'))
 
-    fs.readFile('index.html', function (err, data){
+app.get('/', (req, res) =>{
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+  res.sendFile('./public/index.html', { root: __dirname });
 
-    });
+});
 
-}).listen(3000);
+app.get('/index', (req, res) =>{
+
+  res.sendFile('./public/index.html', { root: __dirname });
+
+});
+
+app.get('/search', (req, res) =>{
+
+  res.sendFile('./public/search.html', { root: __dirname });
+
+});
+
+app.get('/login', (req, res) =>{
+
+  res.sendFile('./public/login.html', { root: __dirname });
+
+});
+
+app.get('/profile', (req, res) =>{
+
+  res.sendFile('./public/profile.html', { root: __dirname });
+
+});
+
+app.get('/create', (req, res) =>{
+
+  res.sendFile('./public/create.html', { root: __dirname });
+
+});
+
+app.get('/delete', (req, res) =>{
+
+  res.sendFile('./public/delete.html', { root: __dirname });
+
+});
+
+app.get('/signup', (req, res) =>{
+
+  res.sendFile('./public/signup.html', { root: __dirname });
+
+});
+
+app.get('/editPost', (req, res) =>{
+
+  res.sendFile('./public/editPost.html', { root: __dirname });
+
+});
+
+app.get('/editProfile', (req, res) =>{
+
+  res.sendFile('./public/editProfile.html', { root: __dirname });
+
+});
+
+app.get('/viewpost', (req, res) =>{
+
+  res.sendFile('./public/viewpost.html', { root: __dirname });
+
+});
