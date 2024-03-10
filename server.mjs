2b39ced95c8448d26d0e92af70c5c6d7d10873fa
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2024-03-10 19:33:48 +0800

    node js connect to MongoDB

--- server.mjs
+++ server.mjs
@@ -1,9 +1,33 @@
 import express from "express";
 import path from 'path';
+import {MongoClient,ServerApiVersion} from 'mongodb';
 
 const app = express();
 const port = 3000;
 const __dirname = path.resolve();
+const uri = "mongodb+srv://ZTORlIlsVr6pGCNc:ZTORlIlsVr6pGCNc@cluster0.bvg7nag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
