cbebd161b86e318954a9089673a0c5e838a19eda
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2024-03-10 15:39:17 +0800

    npm install for mongodb

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
 import express from "express";
+import path from 'path';
 
 const app = express();
 const port = 3000;
-import path from 'path';
 const __dirname = path.resolve();
 
 app.listen(port, () => {
