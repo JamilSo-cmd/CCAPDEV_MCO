c1899ebbb56386c1444a4930cea74c83d98bbed7
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2024-03-24 19:15:19 +0800

    changes in search.html and index.html

--- public/loadPosts.js
+++ public/loadPosts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     
     $.get("/posts", function(data, status){
-        // console.log(data);
 
-        for (post of data){
+        data.forEach((post,x) => {
+            
             console.log(post);
 
             const newPost= $("#postTemplate").clone();
@@ -11,14 +11,14 @@ $(document).ready(function () {
             newPost.find(".date").text(post.date);
             newPost.find(".subject").text(post.subject);
             newPost.find(".message").text(post.message);
+            newPost.find(".pageNum").text("page "+ (data.length-x));
 
             $(".postWindow").prepend(newPost);  
-        
-        }
+        });
         $(".postWindow").append("<div  class='postFooter' style='color: rgb(96, 96, 96);'><p>end of recent history</p></div>");
 
       });
 
-    
+     
 });
 
