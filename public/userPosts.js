ca64f4ca06f35965aba418686e81afa3bf0111ee
Jsf <josef_michael_tan@dlsu.edu.ph>
2024-03-28 21:22:23 +0800

    viewing user's posts works

--- public/userPosts.js
+++ public/userPosts.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
     
+    // gets userToView from url, passed from previous profile.html
+    const userToView = new URLSearchParams(window.location.search).get('userToView');
+    console.log('User to view is: '+ userToView);
+
     $.get("/posts", function(data, status){
 
         data.forEach((post,x) => {
             
-            console.log(post);
+            if(post.author == userToView) {
+                const newPost= $("#postTemplate").clone();
+                newPost.attr('id',"");
+                newPost.find(".username").text(post.author);
+                newPost.find(".icon").attr("src", post.authorPic);
+                newPost.find(".date").text(post.date);
+                newPost.find(".subject").text(post.subject);
+                newPost.find(".message").text(post.message);
+                newPost.find(".pageNum").text("page "+ (data.length-x));
 
-            const newPost= $("#postTemplate").clone();
-            newPost.attr('id',"");
-            newPost.find(".username").text(post.author);
-            newPost.find(".icon").attr("src", post.authorPic);
-            newPost.find(".date").text(post.date);
-            newPost.find(".subject").text(post.subject);
-            newPost.find(".message").text(post.message);
-            newPost.find(".pageNum").text("page "+ (data.length-x));
+                $(".postWindow").prepend(newPost);  
+            }
 
-            $(".postWindow").prepend(newPost);  
         });
         $(".postWindow").append("<div  class='postFooter' style='color: rgb(96, 96, 96);'><p>end of recent history</p></div>");
 
