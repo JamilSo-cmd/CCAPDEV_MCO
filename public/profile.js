411206b7c007a7ed8ec458fdbb893d3664626d6d
Jsf <josef_michael_tan@dlsu.edu.ph>
2024-03-28 22:36:34 +0800

    new stuff

--- public/profile.js
+++ public/profile.js
@@ -37,5 +37,33 @@ $(document).ready(function () {
         .catch(error => {
             console.error('Error fetching user data:', error);
         });
+        
+        // WIP
+        if(!userToView) {
+            
+        }
+
+        $.get("/posts", function(data, status){
+
+            data.forEach((post,x) => {
+                
+                if(post.author == userToView) {
+                    const newPost= $("#postTemplate").clone();
+                    newPost.attr('id',"");
+                    newPost.find(".username").text(post.author);
+                    newPost.find(".username").attr('href', 'profile.html?userToView=' + post.author);
+                    newPost.find(".icon").attr("src", post.authorPic);
+                    newPost.find(".date").text(post.date);
+                    newPost.find(".subject").text(post.subject);
+                    newPost.find(".message").text(post.message);
+                    newPost.find(".pageNum").text("page "+ (data.length-x));
+    
+                    $(".postWindow").prepend(newPost);  
+                }
+    
+            });
+            $(".postWindow").append("<div  class='postFooter' style='color: rgb(96, 96, 96);'><p>end of recent history</p></div>");
+    
+        });
 
 });
